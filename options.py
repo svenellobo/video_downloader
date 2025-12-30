e927946747d56c89ec0d4588ec4421d2ef207486
svenellobo <loborecsven@gmail.com>
2025-12-30 20:38:32 +0100

    created util function for path objects

--- options.py
+++ options.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+
 @dataclass
 class DownloadOptions:
     url: str
-    saving_folder: Path
+    saving_folder: Path 
     audio_only: bool = False
     audio_format: str = "bestaudio"
     video_format: str = "bestvideo"
---
+++ utils.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+def create_path_object(path: str) -> Path:
+    return Path(path)
